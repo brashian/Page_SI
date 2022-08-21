ee7635dc350c2d8a48dad991399022ca510d3275
brashian <bsandovala12@gmail.com>
2022-08-21 14:28:51 -0300

    almost

--- Sitio/main.js
+++ Sitio/main.js
@@ -3,7 +3,7 @@
  //Teniendo encuenta el Y mostrar o no el boton, cambiando el display
     const ocultar_boton = () =>{
         
-        if (window.scrollY > 4000){
+        if (window.scrollY > 2000){
             document.getElementById("btn_subir").style.display = "block"
         }else{
             document.getElementById("btn_subir").style.display = "none"
