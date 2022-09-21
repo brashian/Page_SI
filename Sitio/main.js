4fe2ec44df470fc173af3d215685a820a85c1816
brashian <bsandovala12@gmail.com>
2022-09-21 20:54:08 -0300

    ejemplos animacion

--- Sitio/main.js
+++ Sitio/main.js
@@ -1,19 +1,77 @@
- //Mientras se baja, se ejecuta la funcion
-    window.onscroll = () => {ocultar_boton()}
- //Teniendo encuenta el Y mostrar o no el boton, cambiando el display
-    const ocultar_boton = () =>{
-        
-        if (window.scrollY > 2000){
-            document.getElementById("btn_subir").style.display = "block"
-        }else{
-            document.getElementById("btn_subir").style.display = "none"
-        }
-    }
+//Animacion
+gsap.to("#bg", {
+    scrollTrigger: {
+        scrub: 1
+    },
+    scale: 2,
+})
+
+gsap.to("#man", {
+    scrollTrigger: {
+        scrub: 1
+    },
+    scale: 0.9,
+    
+}) 
+
+gsap.to("#mountain_left", {
+    scrollTrigger: {
+        scrub: 1
+    },
+    x: -4000,
+}) 
+
+gsap.to("#mountain_right", {
+    scrollTrigger: {
+        scrub: 1
+    },
+    x: 4000,
+}) 
+
+gsap.to("#text", {
+    scrollTrigger: {
+        scrub: 1
+    },
+    y: 1500,
+})
+
+gsap.to(".ejemplo", {
+    scrollTrigger: {
+        scrub: 1
+    },
+    scale: 1.5,
+    x: 450,
+    
+}) 
+
+gsap.to(".title", {
+    scrollTrigger: {
+        scrub: 1
+    },
+    
+    x: -450,
+    
+}) 
+//Mientras se baja, se ejecuta la funcion
+window.onscroll = () => {ocultar_boton()}
+//Teniendo encuenta el Y mostrar o no el boton, cambiando el display
+   const ocultar_boton = () =>{
+       
+       if (window.scrollY > 3000){
+           document.getElementById("btn_subir").style.display = "block"
+       }else{
+           document.getElementById("btn_subir").style.display = "none"
+       }
+   }
 //Subir arriba de la pagina
 const subir = () => {
-    window.scrollTo(0,0)
-   
+   window.scrollTo(0,0)
+  
 }
+
+
+
+
 document.getElementById("btn_subir").addEventListener("click",subir)
 
 const cambio_idiomas = () => {
@@ -30,3 +88,4 @@ const cambio_idiomas = () => {
 }
 const idiomas = document.getElementById('option')
 idiomas.addEventListener('change',cambio_idiomas)
+
