45b720e9421ce3c9d6a0b8b58df20efd8c991121
Xaviermora <xaviermoravega12@gmail.com>
2022-09-22 00:09:38 -0300

    T

--- Sitio/main.js
+++ Sitio/main.js
@@ -3,7 +3,7 @@
  //Teniendo encuenta el Y mostrar o no el boton, cambiando el display
     const ocultar_boton = () =>{
         
-        if (window.scrollY > 2000){
+        if (window.scrollY > 200){
             document.getElementById("btn_subir").style.display = "block"
         }else{
             document.getElementById("btn_subir").style.display = "none"
@@ -30,3 +30,31 @@ const cambio_idiomas = () => {
 }
 const idiomas = document.getElementById('option')
 idiomas.addEventListener('change',cambio_idiomas)
+
+gsap.registerPlugin(ScrollTrigger);
+
+let tl2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.div-animacion-astronauta',
+        pin: '.astronauta',
+        scrub: true,
+        start: 'top top',
+        end: '100% 100%',
+    }
+})
+
+tl2.to('.astronauta', {y: '100%', rotate: '360deg'})
+
+let tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: '#semi_circulo',
+        scrub: true,
+        end: '#semi_circulo'
+    }
+});
+
+tl.from('.div-que-son', {x: 400})
+
+tl.from('.fondo', {x: -400}, 0.4)
+
+tl.from('.imagen-underheader', {scale: 0.5}, 0.4)
